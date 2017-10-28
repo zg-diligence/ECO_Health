d315c5569e158d5724b5d7cbcd183bc7ce5c64be
gzhang <zg_hit2015@163.com>
2017-10-28 23:13:29 +0800

    optimize code in view.py and repair the register function

--- ECO/models.py
+++ ECO/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+
 class Disease(models.Model):
     disease_name = models.CharField(max_length=30)
     type = models.CharField(max_length=30)
@@ -11,59 +12,64 @@ class Disease(models.Model):
     def __str__(self):
         return self.disease_name
 
+
 class Symptom(models.Model):
-    symptom_name = models.CharField(max_length=30,default=" ")
-    symptom_infor = models.CharField(max_length=500,default=" ")
+    symptom_name = models.CharField(max_length=30, default=" ")
+    symptom_infor = models.CharField(max_length=500, default=" ")
     diseases = models.ManyToManyField(Disease)
 
     def __str__(self):
         return self.symptom_name
 
+
 class Treatment(models.Model):
-    treatment_name = models.CharField(max_length=30,default=" ")
-    treatment_info = models.CharField(max_length=500,blank=True)
-    type = models.CharField(max_length=30,default=" ")
+    treatment_name = models.CharField(max_length=30, default=" ")
+    treatment_info = models.CharField(max_length=500, blank=True)
+    type = models.CharField(max_length=30, default=" ")
     symptoms = models.ManyToManyField(Symptom)
 
     def __str__(self):
         return self.treatment_name
 
+
 class Evaluation(models.Model):
     treatment = models.ForeignKey(Treatment)
-    cost = models.DecimalField(max_digits=6, decimal_places=2,default=0)
-    choices = (('1','少于1个月'),('2','1-6个月'),('3','6-12个月'),('4','1年以上'))
-    use_time = models.CharField(max_length=2,choices=choices,blank=True)
+    cost = models.DecimalField(max_digits=6, decimal_places=2, default=0)
+    choices = (('1', '少于1个月'), ('2', '1-6个月'), ('3', '6-12个月'), ('4', '1年以上'))
+    use_time = models.CharField(max_length=2, choices=choices, blank=True)
     negative_score = models.IntegerField(default=0)
     positive_score = models.IntegerField(default=0)
     negative_symptoms = models.ManyToManyField(Symptom)
 
     def __str__(self):
-        return str(self.id)
+        return str(str(self.id) + ' ' + self.treatment.treatment_name)
+
 
 class UserInfo(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    #require the dimension of image
-    image = models.ImageField(height_field=50,width_field=50,blank=True)
-    choice_index = (('F','女'),('M','男'))
-    sex = models.CharField(max_length=10,default="M",choices=choice_index)
+    # require the dimension of image
+    image = models.ImageField(height_field=50, width_field=50, blank=True)
+    choice_index = (('F', '女'), ('M', '男'))
+    sex = models.CharField(max_length=10, default="M", choices=choice_index)
     age = models.IntegerField()
-    area = models.CharField(max_length=30,default=" ")
-    person_infor = models.CharField(max_length=300,default=" ")
-    diseases = models.ManyToManyField(Disease,blank=True)
-    symptoms = models.ManyToManyField(Symptom,blank=True)
-    treatments = models.ManyToManyField(Treatment,blank=True)
-    evaluations = models.ManyToManyField(Evaluation,blank=True)
+    area = models.CharField(max_length=30, default=" ")
+    person_infor = models.CharField(max_length=300, default=" ")
+    diseases = models.ManyToManyField(Disease, blank=True)
+    symptoms = models.ManyToManyField(Symptom, blank=True)
+    treatments = models.ManyToManyField(Treatment, blank=True)
+    evaluations = models.ManyToManyField(Evaluation, blank=True)
     is_diagnosed = models.BooleanField(default=False)
 
     def __str__(self):
         return self.user.username
 
+
 class Daily(models.Model):
     date = models.DateField()
-    sleep = models.CharField(max_length=20,default=" ")
-    energe = models.CharField(max_length=20,default=" ")
-    appetite = models.CharField(max_length=20,default=" ")
-    mood = models.CharField(max_length=20,default=" ")
+    sleep = models.CharField(max_length=20, default=" ")
+    energe = models.CharField(max_length=20, default=" ")
+    appetite = models.CharField(max_length=20, default=" ")
+    mood = models.CharField(max_length=20, default=" ")
     weight = models.FloatField()
     exercise_time = models.FloatField()
     user = models.ForeignKey(UserInfo)
@@ -71,11 +77,13 @@ class Daily(models.Model):
     def __str__(self):
         return self.id
 
+
 class Message(models.Model):
     user = models.ForeignKey(User)
     content = models.TextField()
     like = models.IntegerField(default=0)
     post_time = models.DateTimeField(auto_now_add=True)
+
 
 class Comment(models.Model):
     user = models.ForeignKey(User)
@@ -83,31 +91,7 @@ class Comment(models.Model):
     message = models.ForeignKey(Message)
     post_time = models.DateTimeField(auto_now_add=True)
 
+
 class Relation(models.Model):
-    user1 = models.ForeignKey(User,related_name='user1')
-    user2 = models.ForeignKey(User,related_name='user2')
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    user1 = models.ForeignKey(User, related_name='user1')
+    user2 = models.ForeignKey(User, related_name='user2')
--- ECO/views.py
+++ ECO/views.py
@@ -1,7 +1,5 @@
 from django.urls import reverse
-from django.http import Http404
 from django.contrib.auth import authenticate, login, logout
-from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
 from django.views.decorators.csrf import csrf_exempt
 from django.shortcuts import render, render_to_response, \
@@ -11,17 +9,21 @@ import json
 from .method import *
 from .models import *
 
+
 @csrf_exempt
 def page_not_found(request):
     return render_to_response('404.html')
+
 
 @csrf_exempt
 def page_error(request):
     return render_to_response('500.html')
 
+
 @csrf_exempt
 def index(request):
     return render(request, "ECO/index.html")
+
 
 @csrf_exempt
 def user_login(request):
@@ -48,6 +50,7 @@ def user_login(request):
     else:
         return render_to_response('404.html')
 
+
 @csrf_exempt
 def user_register(request):
     if request.method == "POST":
@@ -62,271 +65,189 @@ def user_register(request):
             age = info['age']
             area = info['area']
             try:
-                User.objects.create_user(username, email, password).save()
+                user = User.objects.create_user(username, email, password)
+                user.save()
             except Exception:
-                result = {'status': 'error', 'error_message':'other'}
+                result = {'status': 'error', 'error_message': 'other'}
                 return HttpResponse(json.dumps(result), content_type='application/json')
             else:
-                user = User.objects.get(username=username)
-                user.userinfo_set.create(sex=sex, age=int(age), area=area)
-                result = {'status':'success'}
+                UserInfo(user=user, sex=sex, age=age, area=area).save()
+                result = {'status': 'success'}
                 return HttpResponse(json.dumps(result), content_type='application/json')
         else:
-            result = {'status': 'error', 'error_message':'user_exist'}
+            result = {'status': 'error', 'error_message': 'user_exist'}
             return HttpResponse(json.dumps(result), content_type='application/json')
     else:
         return render_to_response('404.html')
+
 
 @csrf_exempt
 def user_logout(request):
     logout(request)
     return HttpResponseRedirect(reverse('ECO:index'))
 
+
 @csrf_exempt
 def symptom_index(request):
     all_symptom = Symptom.objects.all()
-    context = {'all_symptom':all_symptom}
+    context = {'all_symptom': all_symptom}
     return render(request, 'ECO/symptom_index.html', context)
 
+
 @csrf_exempt
-def symptom_detail(request,symptom_id):
-    the_symptom,disease_and_num,treatment_and_evaluations,counts = load_data_for_symptom_page(symptom_id)
-    context = {}
-    context['the_symptom'] = the_symptom
-    context['disease_and_num'] = disease_and_num
-    context['treatment_and_evaluations'] = treatment_and_evaluations
-    context['counts'] = counts
-    return render(request,'ECO/symptom_detail.html',context)
+def symptom_detail(request, symptom_id):
+    keys = ['the_symptom', 'disease_and_num', 'treatment_and_evaluations', 'counts']
+    values = load_data_for_symptom_page(symptom_id)
+    context = dict(zip(keys, values))
+    return render(request, 'ECO/symptom_detail.html', context)
+
 
 @csrf_exempt
 def disease_index(request):
     all_disease = Disease.objects.all()
-    context = {'all_disease':all_disease}
-    return render(request,'ECO/disease_index.html',context)
+    context = {'all_disease': all_disease}
+    return render(request, 'ECO/disease_index.html', context)
+
 
 @csrf_exempt
-def disease_detail(request,disease_id):
-    the_disease, treatments_for_symptoms,treatments_for_disease,evaluations,ages,diagnosed,undiagnosed,num_men,num_women = load_data_for_disease_page(disease_id)
-    context = {'treatments_for_symptoms':treatments_for_symptoms,'treatments_for_disease':treatments_for_disease}
-    context['evaluations'] = evaluations
-    context['ages'] = ages
-    context['diagnosed'] = diagnosed
-    context['undiagnosed'] = undiagnosed
-    context['num_men'] = num_men
-    context['num_women'] = num_women
-    context['disease'] = the_disease
+def disease_detail(request, disease_id):
+    keys = ['disease', 'treatments_for_symptoms', 'treatments_for_disease',
+            'evaluations', 'ages', 'diagnosed', 'undiagnosed', 'num_men', 'num_women']
+    values = load_data_for_disease_page(disease_id)
+    context = dict(zip(keys, values))
+    context['counts'] = [len(context[keys[1]]), len(context[keys[2]]), len(context[keys[3]])]
+    return render(request, 'ECO/disease_detail.html', context)
 
-    counts = []
-    counts.append(len(treatments_for_symptoms))
-    counts.append(len(treatments_for_disease))
-    counts.append(len(evaluations))
-
-    context['counts'] = counts
-    return render(request,'ECO/disease_detail.html', context)
 
 @csrf_exempt
 def treatment_index(request):
     all_treatment = Treatment.objects.all()
-    context = {'all_treatment':all_treatment}
-    return render(request,'ECO/treatment_index.html',context)
+    context = {'all_treatment': all_treatment}
+    return render(request, 'ECO/treatment_index.html', context)
+
 
 @csrf_exempt
-def treatment_detail(request,treatment_id):
-    the_treatment,target_symptom_and_num,num_side,num_effect,num_cost,num_time,negative_symptoms,counts = load_data_for_treatment_page(treatment_id)
-    context = {}
-    context['the_treatment'] = the_treatment
-    context['target_symptom_and_num'] = target_symptom_and_num
-    context['num_side'] = num_side
-    context['num_effect'] = num_effect
-    context['num_cost'] = num_cost
-    context['num_time'] = num_time
-    context['negative_symptoms'] = negative_symptoms
-    context['counts'] = counts
+def treatment_detail(request, treatment_id):
+    keys = ['the_treatment', 'target_symptom_and_num', 'num_side',
+            'num_effect', 'num_cost', 'num_time', 'negative_symptoms', 'counts']
+    values = load_data_for_treatment_page(treatment_id)
+    context = dict(zip(keys, values))
+    return render(request, 'ECO/treatment_detail.html', context)
 
-    return  render(request,'ECO/treatment_detail.html',context)
 
 @csrf_exempt
+@login_required
 def person_index(request):
-    if not request.user.is_authenticated():
-        return render(request,'ECO/index.html')
+    """person_disease同"""
     user = request.user
     try:
         whole_user = user.userinfo
+        disease_id = whole_user.diseases.all()[0].id
     except IndexError:
         return render_to_response('404.html')
     else:
-        disease_id = whole_user.diseases.all()[0].id
-        context = {}
-        the_disease, treatments_for_symptoms,treatments_for_disease,evaluations,ages,diagnosed,undiagnosed,num_men,num_women = load_data_for_disease_page(disease_id)
-        context = {'treatments_for_symptoms':treatments_for_symptoms,'treatments_for_disease':treatments_for_disease}
-        context['evaluations'] = evaluations
-        context['ages'] = ages
-        context['diagnosed'] = diagnosed
-        context['undiagnosed'] = undiagnosed
-        context['num_men'] = num_men
-        context['num_women'] = num_women
-        context['disease'] = the_disease
-
-        return render(request,'ECO/person_disease.html',context)
+        keys = ['disease', 'treatments_for_symptoms', 'treatments_for_disease',
+                'evaluations', 'ages', 'diagnosed', 'undiagnosed', 'num_men', 'num_women']
+        values = load_data_for_disease_page(disease_id)
+        context = dict(zip(keys, values))
+        return render(request, 'ECO/person_disease.html', context)
 
 
 @csrf_exempt
-def person_disease(request):
-    if not request.user.is_authenticated():
-        return render(request,'ECO/index.html')
-    user = request.user
-    try:
-        whole_user = user.userinfo
-    except IndexError:
-        return render_to_response('404.html')
-    else:
-        disease_id = whole_user.diseases.all()[0].id
-        context = {}
-        the_disease, treatments_for_symptoms,treatments_for_disease,evaluations,ages,diagnosed,undiagnosed,num_men,num_women = load_data_for_disease_page(disease_id)
-        context = {'treatments_for_symptoms':treatments_for_symptoms,'treatments_for_disease':treatments_for_disease}
-        context['evaluations'] = evaluations
-        context['ages'] = ages
-        context['diagnosed'] = diagnosed
-        context['undiagnosed'] = undiagnosed
-        context['num_men'] = num_men
-        context['num_women'] = num_women
-        context['disease'] = the_disease
-
-        return render(request,'ECO/person_disease.html',context)
-
-
-
-@csrf_exempt
+@login_required
 def person_symptom(request):
-    if not request.user.is_authenticated():
-        return render(request,'ECO/index.html')
     user = request.user
     try:
         whole_user = user.userinfo
+        disease_id = whole_user.diseases.all()[0].id
     except IndexError:
         return render_to_response('404.html')
     else:
-        disease_id = whole_user.diseases.all()[0].id
-        context = {}
-        the_disease, treatments_for_symptoms,treatments_for_disease,evaluations,ages,diagnosed,undiagnosed,num_men,num_women = load_data_for_disease_page(disease_id)
-        context = {'treatments_for_symptoms':treatments_for_symptoms,'treatments_for_disease':treatments_for_disease}
-        context['evaluations'] = evaluations
-        context['ages'] = ages
-        context['diagnosed'] = diagnosed
-        context['undiagnosed'] = undiagnosed
-        context['num_men'] = num_men
-        context['num_women'] = num_women
-        context['disease'] = the_disease
-
-        return render(request,'ECO/person_symptom.html',context)
+        keys = ['disease', 'treatments_for_symptoms', 'treatments_for_disease',
+                'evaluations', 'ages', 'diagnosed', 'undiagnosed', 'num_men', 'num_women']
+        values = load_data_for_disease_page(disease_id)
+        context = dict(zip(keys, values))
+        return render(request, 'ECO/person_symptom.html', context)
 
 
 @csrf_exempt
+@login_required
 def person_treatment(request):
-    if not request.user.is_authenticated():
-        return render(request,'ECO/index.html')
     user = request.user
     try:
         whole_user = user.userinfo
+        disease_id = whole_user.diseases.all()[0].id
     except IndexError:
         return render_to_response('404.html')
     else:
-        disease_id = whole_user.diseases.all()[0].id
-        context = {}
-        the_disease, treatments_for_symptoms,treatments_for_disease,evaluations,ages,diagnosed,undiagnosed,num_men,num_women = load_data_for_disease_page(disease_id)
-        context = {'treatments_for_symptoms':treatments_for_symptoms,'treatments_for_disease':treatments_for_disease}
-        context['evaluations'] = evaluations
-        context['ages'] = ages
-        context['diagnosed'] = diagnosed
-        context['undiagnosed'] = undiagnosed
-        context['num_men'] = num_men
-        context['num_women'] = num_women
-        context['disease'] = the_disease
-
-        return render(request,'ECO/person_treatment.html',context)
+        keys = ['disease', 'treatments_for_symptoms', 'treatments_for_disease',
+                'evaluations', 'ages', 'diagnosed', 'undiagnosed', 'num_men', 'num_women']
+        values = load_data_for_disease_page(disease_id)
+        context = dict(zip(keys, values))
+        return render(request, 'ECO/person_treatment.html', context)
 
 
 @csrf_exempt
+@login_required
 def person_info(request):
     pass
 
+
 @csrf_exempt
+@login_required
 def person_update(request):
     pass
 
+
 @csrf_exempt
+@login_required
 def person_diagram(request):
     pass
 
-@csrf_exempt
-def social_index(request):
-    return render(request,'ECO/social_index.html')
 
 @csrf_exempt
+@login_required
+def social_index(request):
+    return render(request, 'ECO/social_index.html')
+
+
+@csrf_exempt
+@login_required
 def social_friendstate(request):
     user = request.user
     userinfo = user.userinfo
     follows = userinfo.you_follow.all()
+    message = [f.message_set.all() for f in follows]
+    context = {'message': message}
+    return render(request, 'ECO/social_friendstate.html', context)
 
-    message = []
-
-    for f in follows:
-        temp = f.message_set.all()
-        message.append(temp)
-
-    context = {'message':message}
-
-    return render(request,'ECO/social_friendstate.html',context)
 
 @csrf_exempt
+@login_required
 def social_friendlist(request):
     user = request.user
     follow_you_relation = user.user2.all()
     you_follow_relation = user.user1.all()
 
-    you_follow = []
-    follow_you = []
+    you_follow = [r.user1 for r in follow_you_relation]
+    follow_you = [r.user2 for r in you_follow_relation]
+    context = {'you_follow': you_follow, 'follow_you': follow_you}
+    return render(request, 'ECO/social_friendlist.html', context)
 
-    for r in follow_you_relation:
-        follow_you.append(r.user1)
-
-    for r in you_follow_relation:
-        you_follow.append(r.user2)
-
-    context = {}
-    context['you_follow'] = you_follow
-    context['follow_you'] = follow_you
-
-    print(you_follow)
-
-    return render(request,'ECO/social_friendlist.html',context)
 
 @csrf_exempt
+@login_required
 def social_sendstate(request):
     pass
 
+
 @csrf_exempt
+@login_required
 def social_newfriend(request):
     pass
 
+
 @csrf_exempt
+@login_required
 def social_heart(request):
     pass
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
